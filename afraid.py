fe5b7b9105f433a8a34b24bf1a3d787ec80db275
Ryan Campbell <campbellr@gmail.com>
2011-08-04 22:12:59 -0600

    add query/update commands as well as clean up output a bit

--- afraid.py
+++ afraid.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 """ This script updates the the afraid.org DDNS server to point to the
     the new current public IP address.
 """
@@ -6,6 +6,8 @@ import urllib2
 import optparse
 import sys
 import pprint
+
+import texttable
 
 try:
     from hashlib import sha1
@@ -18,21 +20,36 @@ def error(msg, code=1):
     print >> sys.stderr, "%s: error: %s" % (sys.argv[0], msg)
     sys.exit(code)
 
+def validate_args(parser, options, args):
+    if len(args) != 1:
+        parser.error("Incorrect number of arguments.")
+
+    action= args[0]
+
+    if action.lower() == "update":
+        required_options = ['username', 'password', 'hostname']
+    elif action.lower() == "query":
+        required_options = ['username', 'password']
+    else:
+        parser.error("Either 'update' or 'query' must be provided.")
+
+    for required_opt in required_options:
+        if not getattr(options, required_opt, None):
+            parser.error("The --%s option must be provided." % required_opt)
+
 def parse_args():
-    parser = optparse.OptionParser()
+    parser = optparse.OptionParser(usage="%prog update|query [-h] [-u <username>] [-p <password>] [-n <hostname>]")
     parser.add_option('-u', '--username', help="The freedns.afraid.org username",
                       dest="username", default=None)
     parser.add_option('-p', '--password', help="The associated password",
                       dest='password', default=None)
-    parser.add_option('-n', '--hostname', help="The name of the host to update",
+    parser.add_option('-n', '--hostname', help="The name of the host to update or query",
                        dest='hostname', default=None)
 
     options, args = parser.parse_args()
-    for required_opt in ['username', 'password', 'hostname']:
-        if not getattr(options, required_opt, None):
-            error("the --%s option must be provided." % required_opt)
+    validate_args(parser, options, args)
 
-    return options
+    return options, args[0]
 
 def get_sha(username, password):
     to_hash = "|".join([username, password])
@@ -70,12 +87,44 @@ def get_record_by_desc(records, desc):
 
     error("No record with description '%s'" % desc)
 
-def main():
-    opts = parse_args()
+def perform_update(opts):
     sha_hash = get_sha(opts.username, opts.password)
     records = get_records(sha_hash)
     record = get_record_by_desc(records, opts.hostname)
     update_url(record)
+
+def print_record(record):
+    for key, value in record.iteritems():
+        print "%s:\t%s" % (key, value)
+
+    print "\n",
+
+def print_results(records):
+    if not isinstance(records, list):
+        records = [records]
+
+    print "\n",
+    for record in records:
+        print_record(record)
+
+
+def perform_query(opts):
+    sha_hash = get_sha(opts.username, opts.password)
+    result = get_records(sha_hash)
+    if opts.hostname:
+        result = get_record_by_desc(result, opts.hostname)
+
+    print_results(result)
+
+def perform_action(action, opts):
+    if action == "query":
+        perform_query(opts)
+    elif action == "update":
+        perform_update(opts)
+
+def main():
+    opts, action = parse_args()
+    perform_action(action, opts)
 
 if __name__ == "__main__":
     main()
